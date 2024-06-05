4e8de456204c2d76ab480cfa76d3e66cdae7e044
rm-a0 <michalrepcik9@gmail.com>
2024-06-05 17:00:54 +0200

    adjusted main

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,10 @@
 
 int main(void) {
     //sf::RenderWindow window(sf::VideoMode(800, 600), "Particle System Test");
-    Vector3 pos(100.0f, 100.0f, 0.0f);
-    Vector3 vel(10.0f, 0.0f, 0.0f);
-    Vector3 acc(0.0f, 1.0f, 0.0f);
-    float lifespan = 5.0f;
+    Vector3 pos(100.0, 100.0);
+    Vector3 vel(10.0);
+    Vector3 acc(0.0, 1.0);
+    float lifespan = 5.0;
 
     Particle particle(pos, vel, acc, lifespan);
 
