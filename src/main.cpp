ab6b272c8cdd7c863a17a65dbcc55de6b43813d5
rm-a0 <michalrepcik9@gmail.com>
2024-06-07 19:02:00 +0200

    tested and added particle handling in main

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,13 @@
 #include "Renderer.h"
 
 int main(void) {
-    Particle particle(Vector3(50.0, 20.0), Vector3(), Vector3(), 500.0, 20.0, Color(100.0, 100.0, 100.0));
+    Particle particle(Vector3(), Vector3(1.0, 1.0, -1.0), Vector3(), 500.0, 20.0, Color(1.0, 1.0, 1.0, 0.5));
     Renderer renderer(800, 600);
 
     // Main loop
     while (!glfwWindowShouldClose(renderer.window)) {
 
-        particle.update(0.1f);
+        particle.update(0.002f);
         renderer.renderParticle(particle);
         glfwPollEvents();
     }
