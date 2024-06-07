65ee4a945a81bcc96955630e82485cf2a321f26c
rm-a0 <michalrepcik9@gmail.com>
2024-06-07 14:27:09 +0200

    refactored main

--- src/main.cpp
+++ src/main.cpp
@@ -3,39 +3,44 @@
 #include <GLFW/glfw3.h>
 #include "Particle.h"
 
-void initialize() {
+void init3D() {
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
 }
 
-int main(void) {
-    Vector3 pos(100.0, 100.0);
-    Vector3 vel(10.0);
-    Vector3 acc(0.0, 1.0);
-    float lifespan = 5.0;
-
-    Particle particle(pos, vel, acc, lifespan, Color(0.0, 1.1, 2.2));
-
+GLFWwindow* initWindow() {
     if (!glfwInit()) {
-        std::cerr << "Failed to initialize GLFW" << std::endl;
-        return -1;
+            std::cerr << "Failed to initialize GLFW" << std::endl;
+            return NULL;
     }
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "3D Particle System", nullptr, nullptr);
     if (!window) {
         std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
-        return -1;
+        return NULL;
     }
 
     glfwMakeContextCurrent(window);
 
     if (glewInit() != GLEW_OK) {
         std::cerr << "Failed to initialize GLEW" << std::endl;
-        return -1;
+        return NULL;
     }
 
-    initialize();
+    init3D();
+
+    return window;
+}
+
+int main(void) {
+    GLFWwindow* window;
+
+    Particle particle(Vector3(), Vector3(), Vector3(), 5.0, Color());
+
+    if ((window = initWindow()) == NULL) {
+        return 1;
+    }
 
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
