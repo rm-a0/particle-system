3beef605bd9c03f61c9432ce07765c2cbc25cf95
rm-a0 <michalrepcik9@gmail.com>
2024-06-07 16:09:20 +0200

    adjusted main

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,13 @@
 #include <GLFW/glfw3.h>
 #include "Particle.h"
 
+// Create 3d space and enable depth
 void init3D() {
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
 }
 
+// Create window
 GLFWwindow* initWindow() {
     if (!glfwInit()) {
             std::cerr << "Failed to initialize GLFW" << std::endl;
@@ -35,13 +37,13 @@ GLFWwindow* initWindow() {
 
 int main(void) {
     GLFWwindow* window;
-
     Particle particle(Vector3(), Vector3(), Vector3(), 5.0, Color());
 
     if ((window = initWindow()) == NULL) {
         return 1;
     }
 
+    // Main loop
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -52,7 +54,6 @@ int main(void) {
 
     glfwDestroyWindow(window);
     glfwTerminate();
-
     return 0;
 }
 
