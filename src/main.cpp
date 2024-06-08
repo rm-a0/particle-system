f7be17fca6e9c800f9fbf9245446c8d4f998c6e8
rm-a0 <michalrepcik9@gmail.com>
2024-06-08 19:12:34 +0200

    tested with more particles

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <random>
 #include "Particle.h"
 #include "Renderer.h"
 
@@ -7,10 +8,25 @@ int main(void) {
     Renderer renderer(800, 600);
 
     std::vector<Particle> particles;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> vel_dist(0.4, 1.8);
+    std::uniform_real_distribution<> vel_dist2(-0.8, 0.8);
+    std::uniform_real_distribution<> life_dist(2.0, 5.0);
+    std::uniform_real_distribution<> pos_dist(-1.0, 1.0);
+    std::uniform_real_distribution<> clr_dist(0.0, 0.5);
+    std::uniform_real_distribution<> clr_dist2(0.8, 1.0);
 
-    particles.push_back(Particle(glm::vec3(3.0f, 3.0f, 0.0f), glm::vec3(0.0f, 0.0f, -5.0f), glm::vec3(0.0f, 0.0f, 0.0f), 500.0, 0.2, Color(0.5, 0.2, 0.9)));
-    particles.push_back(Particle(glm::vec3(-3.0f, -3.0f, 0.0f), glm::vec3(0.0f, 0.0f, -5.0f), glm::vec3(0.0f, 0.0f, 0.0f), 500.0, 0.2, Color(0.5, 0.2, 0.9)));
-
+    for (int i = 0; i < 1000; ++i) {
+        float y_vel = vel_dist(gen);
+        float x_vel = vel_dist2(gen);
+        float z_vel = -vel_dist(gen);
+        float lifetime = life_dist(gen);
+        float x_pos = pos_dist(gen);
+        float g_val = clr_dist(gen);
+        float r_val = clr_dist2(gen);
+        particles.emplace_back(glm::vec3(x_pos, -2.0f, 0.0f), glm::vec3(x_vel, y_vel, z_vel), glm::vec3(0.0f), lifetime, 0.1f, Color(r_val, g_val));
+    }
     // Main loop
     while (!glfwWindowShouldClose(renderer.window)) {
 
